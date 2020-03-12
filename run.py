f3e0af7aba23103732c83ba9e0770f26448ed3f9
ohchangsuk <ocs13@ks.ac.kr>
2020-03-12 16:23:07 +0900

    test

--- run.py
+++ run.py
@@ -1,12 +1,17 @@
-#실제 서비스
+# #실제 서비스
 
-from flask import Flask
+# from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'aws 홈페이지'
+# @app.route('/')
+# def home():
+#     return 'aws 홈페이지'
 
-if __name__=='__main__':
-    app.run(debug=True)
+# if __name__=='__main__':
+#     app.run(debug=True)
+import os
+from bs_corona.BusanCorona import wsgi
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BusanCorona.settings')
+app = get_wsgi_application()
--- wsgi.py
+++ wsgi.py
@@ -1,21 +1,14 @@
-# '''
-# 아파치:apache(or NGINX) 서버 (웹 서버)가 가장 먼저 찾는 파일
-# wsgi기능은 서버기능을 제공하는 역활
-# '''
-# import sys, os
+'''
+아파치:apache(or NGINX) 서버 (웹 서버)가 가장 먼저 찾는 파일
+wsgi기능은 서버기능을 제공하는 역활
+'''
+import sys, os
 
-# cur_dir=os.getcwd
+cur_dir=os.getcwd
 
-# #웹은, 접속로그, 에러로그 존재
-# #로그의 위치를 조정
-# sys.stdout = sys.stderr
-# sys.path.insert(0, cur_dir)
+#웹은, 접속로그, 에러로그 존재
+#로그의 위치를 조정
+sys.stdout = sys.stderr
+sys.path.insert(0, cur_dir)
 
-# from run import app as application
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BusanCorona.settings')
-
-application = get_wsgi_application()
+from run import app as application
